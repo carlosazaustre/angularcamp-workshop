bfb5e78f18039a40909c32d93d11598f0ccaa67d
Carlos Azaustre <cazaustre@gmail.com>
2016-06-27 08:17:46 +0200

    add index.html to SPA

--- server/index.js
+++ server/index.js
@@ -3,15 +3,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const path = require('path')
+const logger = require('morgan')
 
 const app = express()
 
+app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(methodOverride())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.status(200).json({ saludo: 'Hola' })
+app.get('*', (req, res, next) => {
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  } else {
+    next()
+  }
 })
 
 app.listen(3000, () => console.log('Express running on port 3000'))
